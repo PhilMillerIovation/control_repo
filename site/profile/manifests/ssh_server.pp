03407e33b4d6cf7a7511cc9f3eaaf4dead56e40e
Phil Miller <46614413+PhilMillerIovation@users.noreply.github.com>
2019-01-25 15:00:16 -0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
                 ensure => present,
                 user   => 'root',
                 type   => 'ssh-rsa',
-                key    => '',
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCztyREyl7rLsZnO4+OT1GpBhTiCRXtM4GT5m8TFwVgeV4F70plrQsBXgk4VhVkjiFn5H4byOu2zdxbX10JSa89Hdrx+UOBVtJgRu0yqtIoIxQxdfk1TbPSatF3Gi3SlPzTVTCY3MUgST4Cat7PX2O9Ami2ydslRMiyGb+C218Y7sCcTxdtJY4j/EIaVKALEb6FY4S58edBy6oFa67UTLjOs5Cjqck7Cqcu+hPkEE6/+8djNx4jsRigDQlDybOcK5ouucnUrdtwldzws7cQETAKpOwSQ5gkpo9HyU6sU51eeM7dBe01DewG3jbuWnHKsp1wOXeMqufXuOV5T4m2lakL',
         }  
 }
